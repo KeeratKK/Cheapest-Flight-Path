af04ae094d77bf87300d849139552e5e1a56b2a8
Keerat Kohli <100006952+KeeratKK@users.noreply.github.com>
2024-04-10 18:24:45 -0400

    Implemented graph API with Dijkstra's and Bellman-Ford

---
+++ FlightGraph.py
@@ -0,0 +1,127 @@
+from queue import PriorityQueue
+from queue import Queue
+import math
+
+class FlightGraph:
+
+    def __init__(self):
+        self.adj = {}
+
+        # Will be used for dijkstra's and bellman ford
+        self.predecessor = {}
+        self.distances = {}
+
+    def getNumberOfCities(self):
+
+        return len(self.adj)
+
+    def addFlight(self, fromEdge, toEdge, flightCost):
+        
+        if fromEdge not in self.adj:
+            self.adj[fromEdge] = []
+        
+        if toEdge not in self.adj:
+            self.adj[toEdge] = []
+
+        self.adj[fromEdge].append((toEdge, flightCost))
+    
+    def isFlight(self, fromEdge, toEdge):
+
+        for destination in self.adj[fromEdge]:
+
+            if(destination[0] == toEdge):
+                return True
+        
+        return False
+
+    def dijkstras(self, source, destination):
+        pq = PriorityQueue()
+
+        pq.put((0, source))
+        self.distances[source] = 0
+        
+        for vertex in self.adj:
+            self.predecessor[vertex] = -1
+
+            if vertex != source:
+                self.distances[vertex] = math.inf
+                pq.put((math.inf, vertex))
+
+        while not pq.empty():
+            
+            shortestCityTuple = pq.get()
+
+            # Because python doesnt support change priority in their PQ, we will have some old values still in the pq
+            # So if we notice an old value, skip it until we get the correct value
+            while(shortestCityTuple[0] != self.distances[shortestCityTuple[1]] and not pq.empty()):
+                shortestCityTuple = pq.get()
+
+            shortestCityDistance = shortestCityTuple[0]
+            shortestCity = shortestCityTuple[1]
+
+            for neighbors in self.adj[shortestCity]:
+
+                flightCost = neighbors[1]
+                neighborName = neighbors[0]
+
+                if self.distances[shortestCity] + flightCost < self.distances[neighborName]:
+                    self.distances[neighborName] = self.distances[shortestCity] + flightCost
+                    self.predecessor[neighborName] = shortestCity
+
+                    pq.put((self.distances[neighborName], neighborName)) 
+            
+        path = []
+        curVertex = destination
+        while self.predecessor[curVertex] != -1:
+            path.append(curVertex)
+            curVertex = self.predecessor[curVertex]
+        
+        path.append(source)
+        path.reverse()
+
+        return path, self.distances[destination]
+
+    def bellmanFord(self, source, destination):
+
+        self.distances[source] = 0
+        updates = False
+
+        for vertex in self.adj:
+            self.predecessor[vertex] = -1
+
+            if vertex != source:
+                self.distances[vertex] = math.inf
+
+        # Dont need to perform |V| times because we know there are no negative weight cycles
+        for v in range(len(self.adj) - 1):
+            
+            for vertex in self.adj:
+
+                for edge in self.adj[vertex]:
+
+                    flightCost = edge[1]
+                    neighborName = edge[0]
+
+                    if self.distances[vertex] != math.inf and self.distances[vertex] + flightCost < self.distances[neighborName]:
+                        self.distances[neighborName] = self.distances[vertex] + flightCost
+                        self.predecessor[neighborName] = vertex
+
+                        updates = True
+
+            if updates == False:
+                break
+        
+        path = []
+        curVertex = destination
+        while self.predecessor[curVertex] != -1:
+            path.append(curVertex)
+            curVertex = self.predecessor[curVertex]
+        
+        path.append(source)
+        path.reverse()
+
+        return path, self.distances[destination]
+
+        
+
+
