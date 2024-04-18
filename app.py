eaddb1fc4298ea9247761d9261cbfe5a556083a6
Keerat Kohli <100006952+KeeratKK@users.noreply.github.com>
2024-04-18 18:39:03 -0400

    Encapsulated the user menu under a form tag to allow the python script access to data from the html form

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from FlightGraph import FlightGraph
 
 app = Flask(__name__)
 
 @app.route('/') 
 def index():
+    return render_template('main.html')
+
+@app.route('/get_data', methods=['POST'])
+def get_data():
+    origin = request.form['originSearchbar']
+    destination = request.form['destinationSearchbar']
+    firstYear = request.form['firstYearDropDown']
+    secondYear = request.form['secondYearDropDown']
+    algorithmChoice = request.form['algorithmDropDown']
+
+    print(origin)
+    print(destination)
+    print(firstYear)
+    print(secondYear)
+    print(algorithmChoice)
+
     return render_template('main.html')
